470ef1073dae103ce455e47d20d271292744253f
Ekow20 <ekown20@gmail.com>
2023-04-20 13:51:48 +0000

    shell

--- execmd.c
+++ execmd.c
@@ -3,16 +3,20 @@
 void execmd(char **argv)
 {
     char *command = NULL;
+    char *actual_command = NULL;
 
     if (argv)
     {
         /* get the command */
         command = argv[0];
 
-        /* execute the command with execve */
-        if (execve(command, argv, NULL) == -1)
+        /* generate the path to this command before passing it to execve */
+        actual_command = get_location(command);
+
+        /* execute the actual command with execve */
+        if (execve(actual_command, argv, NULL) == -1)
         {
             perror("Error:");
-        };
+        }
     }
 }
